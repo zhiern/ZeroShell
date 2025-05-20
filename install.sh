223aec9fde8b2292135edbe7da0f73516a95d1cb
NERD <168670760+zhiern@users.noreply.github.com>
2025-05-20 19:21:54 +0800

    Update and rename ShellCrash to install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # Copyright (C) ZeroShell Project
 
-[ -z "$url" ] && url="https://fastly.jsdelivr.net/gh/oppen321/ZeroShell@master"
+[ -z "$url" ] && url="https://fastly.jsdelivr.net/gh/zhiern/ZeroShell@master"
 type bash &>/dev/null && shtype=bash || shtype=sh
 echo='echo -e'
 [ -n "$(echo -e | grep e)" ] && {
